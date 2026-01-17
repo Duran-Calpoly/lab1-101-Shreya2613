f9c3de73b89198487ab200e952b97f8aa6bffcf3
Shreya2613 <shtangir@calpoly.edu>
2026-01-17 00:52:16 +0000

    completed lab 1

--- lab1_4.py
+++ lab1_4.py
@@ -1,1 +1,8 @@
-pass
+def calculate_average(num1, num2, num3):
+    return (num1+num2+num3)/3
+def add_tax(bill_total):
+    tax= bill_total * 0.10
+    return bill_total + tax
+def greet_user(name):
+    string= "Hello " + name
+    return string
